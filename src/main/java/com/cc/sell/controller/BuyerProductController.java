be6d156805197588eca848efad65cf60d6308363
chencheng <787915859@qq.com>
2019-12-18 11:02:04 +0800

    升级数据库连接驱动

--- src/main/java/com/cc/sell/controller/BuyerProductController.java
+++ src/main/java/com/cc/sell/controller/BuyerProductController.java
@@ -1,9 +1,13 @@
 package com.cc.sell.controller;
 
+import com.cc.sell.VO.ProductInfoVO;
+import com.cc.sell.VO.ProductVO;
 import com.cc.sell.VO.ResultVO;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
 
 /**
  * @author chencheng
@@ -16,6 +20,10 @@ public class BuyerProductController {
     @GetMapping("/list")
     public ResultVO list(){
         ResultVO resultVO = new ResultVO();
+        ProductVO productVO = new ProductVO();
+        ProductInfoVO productInfoVO = new ProductInfoVO();
+        productVO.setProductInfoVOList(Arrays.asList(productInfoVO));
+        resultVO.setData(Arrays.asList(productVO));
         resultVO.setCode(0);
         resultVO.setMsg("成功");
         return resultVO;
